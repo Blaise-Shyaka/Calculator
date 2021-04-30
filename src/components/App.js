5aa4e87e26339775f14731055086cb8449e67aa1
Blaise-Shyaka <blaiseshyaka95@gmail.com>
2021-04-30 23:31:14 +0200

    Add a Button component

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,9 @@ import Display from './Display';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Display />
-    </div>
+    </>
   );
 }
 
---
+++ src/components/Button.js
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+
+function Button({ name }) {
+  return (
+    <button type="button">
+      {' '}
+      { name }
+      {' '}
+    </button>
+  );
+}
+
+Button.propTypes = {
+  name: PropTypes.string.isRequired,
+};
+
+export default Button;
