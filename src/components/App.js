923d8695f6530c9767a91f2b4a4ad7c3a35ed2e5
Blaise-Shyaka <blaiseshyaka95@gmail.com>
2021-04-30 23:19:47 +0200

    add the Display component

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import Display from './Display';
+
+function App() {
+  return (
+    <div className="App">
+      <Display />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Display.js
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types';
+
+function Display({ result }) {
+  return (
+    <div>
+      <p>{result}</p>
+    </div>
+  );
+}
+
+Display.propTypes = {
+  result: PropTypes.string,
+};
+
+Display.defaultProps = {
+  result: '0',
+};
+
+export default Display;
